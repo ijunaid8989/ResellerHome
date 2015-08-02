fa34b571988839136489837a763759815379a1c8
Junaid Farooq <ijunaidfarooq@gmail.com>
2015-08-02 15:39:08 +0500

    Try to design Reseller Home for Tahir Bhai

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome</title>
+	<link rel="stylesheet" type="text/css" href="css/index.css">
+	<link rel="stylesheet" type="text/css" href="css/font-awesome.css">
+</head>
+<body>
+<section class = "ts-home1-hosting-services parallax-section">
+	<div class="ts-overlay"></div>
+	<div class="container">
+		<div class="col-sm-12">
+			<div class="ts-section-title text-center">
+				<h3>Best Small Business Hosting Services</h3>
+			</div>
+			<div class="row">
+				<div class="col-sm-6  ts-home1-block12-left">
+					<div class="ts-feature-item right">
+						<div class="icon-feature">
+							<i class="fa fa-laptop"></i>
+						</div>
+						<div class="info-feature">
+							<h3>
+								<a href="">One-click set-up</a>
+							</h3>
+							<p>Globally evolve e-business niches with best-of-breed technology. Monotonectally iterate backend 
+							infomediaries for excellent manufactured products. Dramatically disseminate</p>
+						</div>
+					</div>
+					<div class="ts-feature-item right">
+						<div class="icon-feature">
+							<i class="fa fa-coffee"></i>
+						</div>
+						<div class="info-feature">
+							<h3>
+								<a href="">Scalability to fit your needs</a>
+							</h3>
+							<p>Globally evolve e-business niches with best-of-breed technology. Monotonectally iterate backend 
+							infomediaries for excellent manufactured products. Dramatically disseminate</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6 right">
+					<div class="ts-feature-item left">
+						<div class="icon-feature">
+							<i class="fa fa-database"></i>
+						</div>
+						<div class="info-feature">
+							<h3><a href="">Unlimited storage and bandwidth</a></h3>
+							<p>Globally evolve e-business niches with best-of-breed technology. Monotonectally iterate backend infomediaries for excellent manufactured products. Dramatically disseminate</p>
+						</div>
+					</div>
+					<div class="ts-feature-item left">
+						<div class="icon-feature">
+							<i class="fa fa-star"></i>
+						</div>
+						<div class="info-feature">
+							<h3><a href="">24/7 secure monitoring</a></h3>
+								<p>Globally evolve e-business niches with best-of-breed technology. Monotonectally iterate backend infomediaries for excellent manufactured products. Dramatically disseminate</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>		
+	</div>
+</section>
+</body>
+</html>
