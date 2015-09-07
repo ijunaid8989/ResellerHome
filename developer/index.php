6196e649d630ebe2ec1ad909a8d2a2c98d71f93a
Junaid Farooq <ijunaidfarooq@gmail.com>
2015-09-07 08:17:29 +0500

    Final one

---
+++ developer/index.php
@@ -0,0 +1,316 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="description" content="description">
+<meta name="keywords" content="keywords">
+<meta name="author" content="BRKOR">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<title>Junaid Farooq | Web Developer and Designer</title>
+
+<!-- google web font-->
+<link href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,400,300,600,700" rel="stylesheet" type="text/css">
+
+<!-- style sheets-->
+<link rel="stylesheet" media="all" href="css/bootstrap.css" type="text/css">
+<link rel="stylesheet" media="all" href="css/custom.css" type="text/css">
+
+<!-- main jquery libraries / others are at the bottom-->
+<script src="js/jquery.min.js " type="text/javascript"></script>
+<script src="js/modernizr.js" type="text/javascript"></script>
+
+</head>
+<body>	
+  	<nav id="spy" class="spy hidden-sm hidden-xs">
+	    <ul>
+	        <li class="active"><a href="#profile">Profile</a></li>
+	        <li><a href="#work-experience">Work experience</a></li>
+	        <li><a href="#education">Education</a></li>
+	        <li><a href="#professional-skills">Skills</a></li>
+	        <li><a href="#interests">Interests</a></li>
+	        <li><a href="#portfolio">Portfolio</a></li>
+	        <li><a href="#contact">Contact</a></li>
+	    </ul>
+	</nav>
+	<nav id="mini-nav" class="spy visible-sm visible-xs">
+    	<ul>
+	        <li class="dropdown">
+	            <a class="dropdown-toggle" data-toggle="dropdown">Menu</a>
+	            <ul class="dropdown-menu">
+	                <li class="active"><a href="#profile">Profile</a></li>
+	                <li><a href="#work-experience">Work experience</a></li>
+	                <li><a href="#education">Education</a></li>
+	                <li><a href="#professional-skills">Skills</a></li>
+	                <li><a href="#interests">Interests</a></li>
+	                <li><a href="#portfolio">Portfolio</a></li>
+	                <li><a href="#contact">Contact</a></li>
+	            </ul>
+	        </li>
+      	</ul>
+	</nav>
+<header id="header">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-8 col-sm-offset-2 text-center">
+                <img src="img/face.jpg" class="face" alt="">
+                <hr class="empty">
+                <p class="header-name">Junaid Farooq</p>
+                <p class="header-title">Web Developer &amp; Web Designer</p>
+                <p class="header-subtitle"><b>Hello my name is Mark William Connor</b> and I'm a Web Designer &amp; Web Developer from Melbourne, Australia. In pharetra orci dignissim, blandit mi semper, ultricies diam. Suspendisse malesuada suscipit nunc non volutpat. Sed porta nulla id orci laoreet tempor non consequat enim. Sed vitae aliquam velit. Aliquam ante erat, blandit at pretium et, accumsan ac est. Integer vehicula rhoncus molestie. Morbi ornare ipsum sed sem condimentum, et pulvinar tortor luctus. Suspendisse condimentum lorem ut elementum aliquam. Mauris nec erat ut libero vulputate pulvinar.</p>
+            </div>
+        </div>
+    </div>
+</header>
+<section id="profile" class="profile activate">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-7 col-sm-offset-5">
+                <ul>
+                    <li class="title"><span class="glyphicon glyphicon-user"></span>Profile</li>
+                    <li>
+                        <span class="note">Full name</span>
+                        <p><strong>Junaid Farooq</strong></p>
+                    </li>
+                    <li>
+                        <span class="note">Born</span>
+                        <p><strong>09 September 1991</strong></p>
+                        <p><strong>Bahawalpur, Pakistan</strong></p>
+                    </li>
+                    
+                    <li>
+                        <span class="note">LinkedIn Profile</span>
+                        <p><strong><a href="#">https://pk.linkedin.com/in/iamjunnii</a></strong></p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
+<section id="work-experience" class="work-experience">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-7 col-sm-offset-5">
+                <ul>
+                    <li class="title"><span class="glyphicon glyphicon-briefcase"></span>Work experience</li>
+                    <li>
+                        <span class="note">June 2012 - Current</span>
+                        <p><strong>IT Group</strong></p>
+                        <p><strong>Lead Web Designer</strong> in Melbourne, AU</p>
+                        <p class="description">Vestibulum ut pharetra diam, vitae hendrerit sapien. Praesent porttitor augue nec semper suscipit. Nulla et cursus lectus. Praesent vel feugiat dolor.</p>
+                    </li>
+                    <li>
+                        <span class="note">September 2010 - Current</span>
+                        <p><strong>Freelance</strong></p>
+                        <p><strong>Lead Web Developer</strong> in Melbourne, AU</p>
+                        <p class="description">Donec vel nulla mollis, mattis orci vel, vulputate tellus. Cras interdum sapien quis risus mattis egestas. Etiam rhoncus lacus turpis, in vulputate arcu imperdiet non.</p>
+                    </li>
+                    <li>
+                        <span class="note">September 2010 - Current</span>
+                        <p><strong>Freelance</strong></p>
+                        <p><strong>Lead Web Developer</strong> in Melbourne, AU</p>
+                        <p class="description">Donec vel nulla mollis, mattis orci vel, vulputate tellus. Cras interdum sapien quis risus mattis egestas. Etiam rhoncus lacus turpis, in vulputate arcu imperdiet non.</p>
+                    </li>
+                    <li>
+                        <span class="note">September 2010 - Current</span>
+                        <p><strong>Freelance</strong></p>
+                        <p><strong>Lead Web Developer</strong> in Melbourne, AU</p>
+                        <p class="description">Donec vel nulla mollis, mattis orci vel, vulputate tellus. Cras interdum sapien quis risus mattis egestas. Etiam rhoncus lacus turpis, in vulputate arcu imperdiet non.</p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
+<section id="education" class="education">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-7 col-sm-offset-5">
+                <ul>
+                    <li class="title"><span class="glyphicon glyphicon-book"></span>Education</li>
+                    <li>
+                        <span class="note">January 2005 - December 2008</span>
+                        <p><strong>Masters Degree</strong></p>
+                        <p><strong>University of IT</strong> in Melbourne, Australia</p>
+                        <p class="description">Vestibulum ut pharetra diam, vitae hendrerit sapien. Praesent porttitor augue nec semper suscipit. Nulla et cursus lectus. Praesent vel feugiat dolor.</p>
+                    </li>
+                    <li>
+                        <span class="note">January 2002 - December 2005</span>
+                        <p><strong>Bachelors Degree</strong></p>
+                        <p><strong>University of IT</strong> in Melbourne, Australia</p>
+                        <p class="description">Donec vel nulla mollis, mattis orci vel, vulputate tellus. Cras interdum sapien quis risus mattis egestas. Etiam rhoncus lacus turpis, in vulputate arcu imperdiet non.</p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
+<section id="professional-skills" class="professional-skills">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-7 col-sm-offset-5">
+                <ul>
+                    <li class="title"><span class="glyphicon glyphicon-tasks"></span>Professional skills</li>
+                    <li>
+                        <p>Adobe InDesign<span class="annotation pull-right">95%</span></p>
+                        <div class="progress">
+                            <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100" style="width: 95%;"></div>
+                        </div>
+                    </li>
+                    <li>
+                        <p>Adobe Photoshop<span class="annotation pull-right">85%</span></p>
+                        <div class="progress">
+                            <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100" style="width: 85%;"></div>
+                        </div>
+                    </li>
+                    <li>
+                        <p>Adobe Illustrator<span class="annotation pull-right">70%</span></p>
+                        <div class="progress">
+                            <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width: 70%;"></div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
+<section id="personal-skills" class="personal-skills">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-7 col-sm-offset-5">
+                <ul>
+                    <li class="title"><span class="glyphicon glyphicon-stats"></span>Personal skills</li>
+                    <li>
+                        <span class="chart" data-percent="95"><span class="skill">Creativity</span>
+                            <span class="percent">95</span>
+                        <canvas height="110" width="110"></canvas></span>
+                        <span class="chart" data-percent="85"><span class="skill">Social skills</span>
+                            <span class="percent">85</span>
+                        <canvas height="110" width="110"></canvas></span>
+                        <span class="chart" data-percent="80"><span class="skill">Leadership</span>
+                            <span class="percent">80</span>
+                        <canvas height="110" width="110"></canvas></span>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
+<section id="interests" class="interests">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-7 col-sm-offset-5">
+                <ul>
+                    <li class="title"><span class="glyphicon glyphicon-heart-empty"></span>Interests</li>
+                    <li>
+                        <p><strong>Books</strong></p>
+                        <p class="description">Vestibulum ut pharetra diam, vitae hendrerit sapien. Praesent porttitor augue nec semper suscipit. Nulla et cursus lectus. Praesent vel feugiat dolor.</p>
+                    </li>
+                    <li>
+                        <p><strong>Art</strong></p>
+                        <p class="description">Donec vel nulla mollis, mattis orci vel, vulputate tellus. Cras interdum sapien quis risus mattis egestas. Etiam rhoncus lacus turpis, in vulputate arcu imperdiet non.</p>
+                    </li>
+                    <li>
+                        <p><strong>Basketball</strong></p>
+                        <p class="description">Maecenas velit eros, congue quis accumsan sed, consequat id magna. Pellentesque at auctor lacus.</p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
+<section id="portfolio" class="portfolio">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-7 col-sm-offset-5">
+                <ul>
+                    <li class="title"><span class="glyphicon glyphicon-eye-open"></span>Portfolio</li>
+                    <li class="static">
+                        <img src="img/1.jpg" alt="" data-toggle="modal" data-target="#modal1">
+                        <img src="img/2.jpg" alt="" data-toggle="modal" data-target="#modal2">
+                        <img src="img/3.jpg" alt="" data-toggle="modal" data-target="#modal3">
+                        <img src="img/4.jpg" alt="" data-toggle="modal" data-target="#modal4">
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
+<div id="modal1" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="modal1" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-box">
+            <img src="img/1.jpg" alt="">
+        </div>
+    </div>
+</div>
+<div id="modal2" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="modal2" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-box">
+            <img src="img/2.jpg" alt="">
+        </div>
+    </div>
+</div>
+<div id="modal3" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="modal3" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-box">
+            <img src="img/3.jpg" alt="">
+        </div>
+    </div>
+</div>
+<div id="modal4" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="modal4" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-box">
+            <img src="img/4.jpg" alt="">
+        </div>
+    </div>
+</div>
+<section id="contact" class="contact">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-7 col-sm-offset-5">
+                <ul>
+                    <li class="title"><span class="glyphicon glyphicon-envelope"></span>Contact</li>
+                    <li class="static">
+                        <span class="note">Address</span>
+                        <p><strong>121 King Street, Melbourne</strong></p>
+                        <p><strong>Victoria 3000 Australia</strong></p>
+                        <span class="note">Mobile</span>
+                        <p><strong>987 654 321</strong></p>
+                        <span class="note">Phone</span>
+                        <p><strong>+61 3 8376 6284</strong></p>
+                        <span class="note">Email</span>
+                        <p><strong><a href="#">mark.william.connor@gmail.com</a></strong></p>
+                        <hr class="empty">
+                        <form action="contact.php" method="post">
+                            <div class="form-group input-group">
+                                <span class="input-group-addon"><span class="glyphicon glyphicon-user"></span></span>
+                                <input name="yourname" type="text" class="form-control" id="inputname" placeholder="Enter your name">
+                            </div>
+                            <div class="form-group input-group">
+                                <span class="input-group-addon"><span class="glyphicon glyphicon-envelope"></span></span>
+                                <input name="email" type="email" class="form-control" id="inputemail" placeholder="Enter your email">
+                            </div>
+                            <div class="form-group input-group">
+                                <span class="input-group-addon"><span class="glyphicon glyphicon-bold"></span></span>
+                                <input name="subject" type="text" class="form-control" id="inputsubject" placeholder="Enter subject">
+                            </div>
+                            <div class="form-group input-group">
+                                <span class="input-group-addon"><span class="glyphicon glyphicon-comment"></span></span>
+                                <textarea name="comments" class="form-control" id="inputmessage" rows="3" placeholder="Enter message here"></textarea>
+                            </div>
+                            <button type="submit" class="btn btn-success pull-right"><span class="glyphicon glyphicon-send"></span> Send message</button>
+                        </form>
+                    </li>
+                    <li class="title up"><span class="glyphicon glyphicon-arrow-up"></span>Up</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/bootstrap-progressbar.js"></script>
+<script src="js/spy.js"></script>
+<script src="js/scripts.js"></script>
+<script src="js/jquery.easypiechart.js"></script>
+<script src="js/chart.js"></script>
+  </body>
+</html>
